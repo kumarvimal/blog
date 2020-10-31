e29c1a62c708747400c2e75325f81cbb3fa9a332
Kumar Vimal <mail@kumarvimal.in>
2020-10-31 19:30:31 +0530

    profille and site config changed

--- components/Bio.js
+++ components/Bio.js
@@ -10,9 +10,9 @@ export default function Bio({ className }) {
     <div className={clsx(`flex items-center`, className)}>
       <Image
         className="flex-shrink-0 mb-0 mr-3 rounded-full w-14 h-14"
-        src={require("../content/assets/profile.png")}
-        webpSrc={require("../content/assets/profile.png?webp")}
-        previewSrc={require("../content/assets/profile.png?lqip")}
+        src={require("../content/assets/profile.jpg")}
+        webpSrc={require("../content/assets/profile.jpg?webp")}
+        previewSrc={require("../content/assets/profile.jpg?lqip")}
         alt="Profile"
       />
       <p className="text-base leading-7">
--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 export default {
   siteMetadata: {
-    title: "Next.js Starter Blog",
+    title: "Logically absurd",
     author: {
-      name: "Jose Felix",
-      summary: "who works building clean user interfaces with React.",
+      name: "Kumar",
+      summary: ", software artisan, maker, absurdist ",
     },
     description: "A blog created with Next.js and Tailwind.css",
     siteUrl: "https://nextjs-starter-blog-demo.netlify.app/",
     language: "en-US",
     social: {
-      twitter: "Jose_R_Felix",
+      twitter: "kumarvimal",
     },
   },
 };
